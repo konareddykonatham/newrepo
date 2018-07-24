e6d369e35b9924117ed56719f212eb89a55b871d
konareddykonatham <39440474+konareddykonatham@users.noreply.github.com>
2018-07-24 14:37:44 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
-sudo yum -y install httpd
-sudo service httpd start  
+sudo apt-get update -y 
+sudo apt-get install apache2 -y 
+sudo systemctl restart apache2
 echo "new technology" >> /var/www/html/index.html
